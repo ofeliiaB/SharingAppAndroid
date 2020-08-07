65f01b0e0205aafc986ccf261e58c1e6ed81c931
Ofeliia <ofeliia.bagratian@gmail.com>
2020-08-07 08:23:47 +0200

    Initial

---
+++ app/src/main/java/com/example/sharingapp/Contact.java
@@ -0,0 +1,47 @@
+package com.example.sharingapp;
+
+import java.util.UUID;
+
+public class Contact {
+    private String username;
+    private String email;
+    private String id;
+
+    public Contact(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+
+        if(this.id ==null){
+            setId(id);
+        } else {
+            updateId(id);
+        }
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(String id) {
+        this.id = UUID.randomUUID().toString();
+    }
+    public void updateId(String id){
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
